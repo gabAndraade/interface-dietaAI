bed40f1291612d6e518d99216fbc2f6be048e624
Gabriel Andrade <160685894+gabAndraade@users.noreply.github.com>
2024-09-24 11:42:57 -0300

    Delete api.ts

--- api.ts
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-export const api = axios.create({
-    baseURL: "https://backend-dieta-ai.vercel.app/",
-    headers: {
-        'Content-Type': 'application/json',
-    },
-    withCredentials: true // Isso garante que os cookies se houver sejam enviados
-});
