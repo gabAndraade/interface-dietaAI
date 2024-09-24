4914557507ff9978815835fe8facd919bcfd6336
Gabriel Andrade <160685894+gabAndraade@users.noreply.github.com>
2024-09-24 11:54:24 -0300

    Add files via upload

--- services/api.ts
+++ services/api.ts
@@ -1,6 +1,5 @@
 import axios from 'axios'
 
-// http://192.168.1.16:3333 / create
 export const api = axios.create({
-    baseURL: "https://backend-dieta-ai.vercel.app/"
-})
+    baseURL: "https://backend-dieta-ai.vercel.app/",
+});
