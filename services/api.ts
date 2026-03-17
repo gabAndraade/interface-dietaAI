498dc0028311581eb4c295da6e936cb34da72c64
Gabriel Andrade <160685894+gabAndraade@users.noreply.github.com>
2026-03-17 14:01:36 -0300

    Update API base URL for axios instance

--- services/api.ts
+++ services/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const api = axios.create({
-    baseURL: "https://backend-dietaai-8jzq2c5ex-gabandraades-projects.vercel.app/",
+    baseURL: "https://backend-dietaai.vercel.app/create",
 });
